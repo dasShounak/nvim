8a770541d794fb0f3e4797683fa666c27843f375
Shounak Das <shounakdas@protonmail.com>
2023-03-18 12:36:12 +0530

    bufferline config update

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,11 +1,18 @@
 require("bufferline").setup{
 	options = {
-		separator_style = 'slant',
+  		separator_style = 'slope',
+
 		hover = {
 			enabled = true,
-			delay = 200,
+			delay = 0,
 			reveal = {'close'}
 		},
+
+		diagnostics = 'nvim-lsp',
+		diagnostics_indicator = function(count, level, diagnostics_dict, context)
+			local icon = level:match("error") and " " or " "
+			return " " .. icon .. count
+		end,
 
 	},
 }
