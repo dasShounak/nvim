96c85ad2b2d48c714cef923e509cfa3a48c72928
Shounak Das <shounakdas@protonmail.com>
2023-03-18 13:07:30 +0530

    Added keybindings for NvimTree

--- lua/shounak/keybindings.lua
+++ lua/shounak/keybindings.lua
@@ -6,20 +6,23 @@ function map(m, k, v)
 	vim.keymap.set(m, k, v, {silent = true})
 end
 
-map('n', '<C-b>', vim.cmd.Ex)
-
 map('i', 'jk', '<Esc>')
 map('n', '<C-s>', ':w<CR>')
 map('n', '<C-q>', ':q<CR>')
 
 map('n', '<leader><leader>', ':so<CR>')
 
--- Telescope keybindings
+-- Telescope --
 map('n', '<leader>ff', ts.find_files, {})
 map('n', '<leader>fg', ts.live_grep, {})
 map('n', '<leader>fb', ts.buffers, {})
 map('n', '<leader>fh', ts.help_tags, {})
 
--- Bufferline keybindings
+-- Bufferline --
 map('n', '<Tab>', ':BufferLineCycleNext<CR>')
 map('n', '<S-Tab>', ':BufferLineCyclePrev<CR>')
+map('n', '<leader>x', ':bdelete %<CR>')
+
+-- NvimTree --
+map('n', '<C-b>', ':NvimTreeToggle<CR>')
+map('n', '<C-e>', ':NvimTreeFocus<CR>')
