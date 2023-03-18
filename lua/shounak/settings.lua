24908cca49e5f741aab587dc3856460c498e8ab0
Shounak Das <shounakdas@protonmail.com>
2023-03-18 13:07:48 +0530

    Disabled netrw

--- lua/shounak/settings.lua
+++ lua/shounak/settings.lua
@@ -1,3 +1,6 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Set Colorscheme
 vim.cmd("colorscheme catppuccin")
 
@@ -14,3 +17,5 @@ v.mousemev = true
 v.tabstop = 4
 v.shiftwidth = 4
 v.expandtab = true
+
+v.scrolloff = 8
